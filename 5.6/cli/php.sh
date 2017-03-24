2edf1ae39b0dc69c184fd3e0c8c4d22e835d1ad7
Niklas Funke <info@niklasfunke.de>
2017-03-24 10:40:56 +0100

    Updated PHP to 7.0.17, 7.1.3 Fixed an issue with PHP7.1 image

--- 5.6/cli/php.sh
+++ 5.6/cli/php.sh
@@ -22,7 +22,7 @@ if ${PHP_XDEBUG_ENABLED}; then
     fi
 
     exec php-cli ${defaultSettings} \
-        -dzend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20151012/xdebug.so \
+        -dzend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20131226/xdebug.so \
         -dhtml_errors=false \
         -dxdebug.remote_enable=1 \
         -dxdebug.remote_autostart=1 \
--- 5.6/fpm/docker-entrypoint.sh
+++ 5.6/fpm/docker-entrypoint.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
 # Xdebug settings
-PHP_XDEBUG_ENABLED=${PHP_XDEBUG_ENABLED:-false}
-PHP_XDEBUG_REMOTE_HOST=${PHP_XDEBUG_REMOTE_HOST:-""}
+export PHP_XDEBUG_ENABLED=${PHP_XDEBUG_ENABLED:-false}
+export PHP_XDEBUG_REMOTE_HOST=${PHP_XDEBUG_REMOTE_HOST:-""}
+export PHP_XDEBUG_IDEKEY=${PHP_XDEBUG_IDEKEY:-"dockerizr-php"}
 
 # Default INI settings.
-PHP_INI_MEMORY_LIMIT=${PHP_INI_MEMORY_LIMIT:-1G}
-PHP_INI_MAX_EXECUTION_TIME=${PHP_INI_MAX_EXECUTION_TIME:-30}
+export PHP_INI_MEMORY_LIMIT=${PHP_INI_MEMORY_LIMIT:-1G}
+export PHP_INI_MAX_EXECUTION_TIME=${PHP_INI_MAX_EXECUTION_TIME:-30}
 
-defaultSettings="\
-    -dmemory_limit=${PHP_INI_MEMORY_LIMIT} \
-    -dmax_execution_time=${PHP_INI_MAX_EXECUTION_TIME} \
-"
+defaultSettings=$(php-cli /usr/local/bin/settings.php)
 
 if ${PHP_XDEBUG_ENABLED}; then
 
@@ -23,15 +21,15 @@ if ${PHP_XDEBUG_ENABLED}; then
         exit 1
     fi
 
-    exec php-fpm \
-        -dzend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20151012/xdebug.so \
+    exec php-fpm ${defaultSettings} \
+        -dzend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20131226/xdebug.so \
         -dhtml_errors=false \
         -dxdebug.remote_enable=1 \
         -dxdebug.remote_autostart=1 \
         -dxdebug.remote_connect_back=1 \
         -dxdebug.remote_host=$PHP_XDEBUG_REMOTE_HOST \
-        -dxdebug.idekey="phpstorm" \
-        -dxdebug.default_enable=off ${defaultSettings} \
+        -dxdebug.idekey=$PHP_XDEBUG_IDEKEY \
+        -dxdebug.default_enable=off \
         "$@"
 
 else
