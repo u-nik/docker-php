61799d457cdbb5a1426c6aedcda31e37a5736d84
Niklas Funke <info@niklasfunke.de>
2017-09-18 17:07:18 +0200

    Updated docker-entrypoint.sh and php.sh to be the same in all versions.

--- 7.0/cli/php.sh
+++ 7.0/cli/php.sh
@@ -21,15 +21,15 @@ if ${PHP_XDEBUG_ENABLED}; then
         echo "INFO: No remote host for xdebug found. Use PHP_XDEBUG_REMOTE_HOST env variable to set it"
     fi
 
-    exec php-cli ${defaultSettings} \
+    exec php-cli \
         -dzend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20151012/xdebug.so \
         -dhtml_errors=false \
         -dxdebug.remote_enable=1 \
         -dxdebug.remote_autostart=1 \
-        -dxdebug.remote_connect_back=1 \
+        -dxdebug.remote_connect_back=0 \
         -dxdebug.remote_host=$PHP_XDEBUG_REMOTE_HOST \
         -dxdebug.idekey=$PHP_XDEBUG_IDEKEY \
-        -dxdebug.default_enable=off \
+        -dxdebug.default_enable=off ${defaultSettings} \
         "$@"
 
 else
--- 7.1/fpm/php.sh
+++ 7.1/fpm/php.sh
@@ -7,6 +7,7 @@ export PHP_XDEBUG_IDEKEY=${PHP_XDEBUG_IDEKEY:-"dockerizr-php"}
 
 # Default INI settings.
 export PHP_INI_MEMORY_LIMIT=${PHP_INI_MEMORY_LIMIT:-1G}
+export PHP_INI_MAX_EXECUTION_TIME=${PHP_INI_MAX_EXECUTION_TIME:-0}
 
 # Create on demand php settings.
 defaultSettings=$(php-cli /usr/local/bin/settings.php)
@@ -20,15 +21,15 @@ if ${PHP_XDEBUG_ENABLED}; then
         echo "INFO: No remote host for xdebug found. Use PHP_XDEBUG_REMOTE_HOST env variable to set it"
     fi
 
-    exec php-cli ${defaultSettings} \
+    exec php-cli \
         -dzend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20160303/xdebug.so \
         -dhtml_errors=false \
         -dxdebug.remote_enable=1 \
         -dxdebug.remote_autostart=1 \
-        -dxdebug.remote_connect_back=1 \
+        -dxdebug.remote_connect_back=0 \
         -dxdebug.remote_host=$PHP_XDEBUG_REMOTE_HOST \
         -dxdebug.idekey=$PHP_XDEBUG_IDEKEY \
-        -dxdebug.default_enable=off \
+        -dxdebug.default_enable=off ${defaultSettings} \
         "$@"
 
 else
